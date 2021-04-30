289bddce560edb14623506d9e6343eed017c7f75
Maxim <maksim.dubko99@gmail.com>
2021-04-30 16:03:40 +0300

    End main page
    End all blocks
    1. sell event carousel
    2. category carousel
    3. flash sale carousel
    4. mega sale carousel
    5. banner
    6. main product list
    7. bottom menu

---
+++ js/app.js
@@ -0,0 +1,9 @@
+$(document).ready(function(){
+    $(".single-itemCarousel").owlCarousel({
+        items: 1,
+        dots: true,
+        dotsEach: true,
+        dotClass: "customDots",
+        margin: 10,
+    });
+  });
