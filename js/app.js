19fa8de068b58fddbec41f11b456bd629b9ed1b9
Maxim <maksim.dubko99@gmail.com>
2021-05-07 16:26:47 +0300

    added review page

--- js/app.js
+++ js/app.js
@@ -15,3 +15,14 @@ $(document).ready(function () {
     margin: 10,
   });
 });
+
+let vh = window.innerHeight * 0.01;
+document.documentElement.style.setProperty("--vh", `${vh}px`);
+(function init100vh() {
+  function setHeight() {
+    var vh = window.innerHeight * 0.01;
+    document.documentElement.style.setProperty("--vh", `${vh}px`);
+  }
+  setHeight();
+  window.addEventListener("resize", setHeight);
+})();
